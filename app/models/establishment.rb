c2c58450712269267225574db979c2ba054a68da
Alex Knoll & srgupta <pair+alexandergknoll.srgupta@devbootcamp.com>
2015-04-13 16:32:09 -0500

    added category association to establishment model

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,6 +2,7 @@ class Establishment < ActiveRecord::Base
   # Remember to create a migration!
   has_many :reviews
   belongs_to :city
+  belongs_to :category
   has_many :patrons, through: :reviews, source: :user
   has_one :state, through: :city
 end
